d59c39f5892abb8c15d6164caa772068ce4f1e75
carloschicas <carlos.chicas@503consulting.com>
2021-07-19 20:49:43 -0600

    Add controller restrictions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  before_action :logged_in_redirect, only: [:new, :create]
 
   def new
   end
@@ -26,6 +27,13 @@ class SessionsController < ApplicationController
 
   def session_params
     params.require(:session).permit(:username, :password)
+  end
+
+  def logged_in_redirect
+    if logged_in?
+      flash[:error] = 'You are already logged in'
+      redirect_to root_path
+    end
   end
 
 end
